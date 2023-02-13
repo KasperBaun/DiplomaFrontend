5a68dc9ac004956e7ff8b63f951fea3ea47f83cd
Simon Fridolf <fridolf.lindhard@gmail.com>
2023-02-13 12:19:13 +0100

    Create categories model

---
+++ src/models/categories.ts
@@ -0,0 +1,11 @@
+export default class categories{
+    title : string = ""; 
+    picture : string = "";  
+
+    constructor(
+        title : string, 
+        picture : string){
+            this.title = title;
+            this.picture = picture;
+    }
+}
--- src/pages/webshop/categories/CategoriesPage.tsx
+++ src/pages/webshop/categories/CategoriesPage.tsx
@@ -1,18 +1,25 @@
 import { observer } from "mobx-react-lite"
 import { useParams } from "react-router-dom";
+import categories from "../../../models/categories";
 
 interface ICategoriesPageProps {
-
+    categoryList?: categories[]
 }
 
 const CategoriesPage: React.FC<ICategoriesPageProps> = observer(function Categories(props: ICategoriesPageProps) {
     const { number } = useParams();
     return (
         <div>
-            <h1>CategoriesPage</h1>
-            {number && <p>
-                The number is  {number}</p>
-            }
+            <div>
+
+            </div>
+
+            <div>
+                <h1>CategoriesPage</h1>
+                {number && <p>
+                    The number is  {number}</p>
+                }
+            </div>
         </div>
     )
 
