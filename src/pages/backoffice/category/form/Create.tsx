49c9497974ea50047d87e6a3c5e335baa5b78519
Kasper Baun <kasperbaun@hotmail.com>
2023-02-27 11:28:44 +0100

    minor changes and testing of create category func

--- src/pages/backoffice/category/form/Create.tsx
+++ src/pages/backoffice/category/form/Create.tsx
@@ -2,8 +2,6 @@ import Category from "@models/Category";
 import MobXContext from "@stores/MobXContext";
 import { useContext, useState } from "react";
 import { Button, Form } from "react-bootstrap";
-import Localization from '@utils/Localization';
-
 
 const CreateForm = () => {
 
@@ -13,28 +11,16 @@ const CreateForm = () => {
     const [url, setUrl] = useState<string>("");
     const [order, setOrder] = useState<number>(0);
     const [description, setDescription] = useState<string>("");
-    const [products, setProducts] = useState<string[]>([]);
-    const [subCategories, setSubCategories]= useState<string[]>([]);
 
-    // order?: number;
-    // imageUrl?: string;
-    // description?: string;
-    // products?: string[];
-    // subCategories?: string[];
+    function createCategory() {
+        let category: Category = {
+            id: 0,
+            name: title,
+            imageUrl: url,
+            order: order,
+            description: description
+        };
 
-    // const category: Category = {
-    //     id: 1,
-    //     imageUrl: "https://via.placeholder.com/150",
-    //     name: "Test placeholder",
-    //     order:1,
-    //     description: "test"
-    // };
-
-    function createCategory(title : string, url : string) {
-        let category : Category = ({ id: 0, name: title, imageUrl: url })
-        console.log(title)
-        console.log(url)
-        console.log(category)
         const createCategoryAsync = async () => {
             try {
                 await categoryStore.createCategory(category)
@@ -76,7 +62,7 @@ const CreateForm = () => {
                     setDescription(temp);
                 }} />
             </Form.Group>
-            <Form.Group>
+            {/* <Form.Group>
                 <Form.Label>Products</Form.Label>
                 <Form.Control type="text" placeholder={Localization.Handler("ProductCommaSeperatedText", localStorage.getItem('locale'))} onChange={(event) => {
                     let temp = event.target.value;
@@ -84,8 +70,8 @@ const CreateForm = () => {
                 }} />
                 <Form.Text muted>
                 </Form.Text>
-            </Form.Group>
-            <Button variant="primary" type="submit" style={{ marginTop: "0.5rem" }} onClick={() => createCategory(title, url)}>
+            </Form.Group> */}
+            <Button variant="primary" type="submit" style={{ marginTop: "0.5rem" }} onClick={createCategory}>
                 Submit
             </Button>
         </Form>
