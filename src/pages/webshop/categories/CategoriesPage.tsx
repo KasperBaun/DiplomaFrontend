aa4833f5af9158776f24e5b1d8bb8025e14d0fb5
Simon Fridolf <fridolf.lindhard@gmail.com>
2023-03-02 13:02:50 +0100

    Intial attempts at subCategories and onclick

--- src/pages/webshop/categories/CategoriesPage.tsx
+++ src/pages/webshop/categories/CategoriesPage.tsx
@@ -2,7 +2,8 @@ import { observer } from "mobx-react-lite"
 import { useContext, useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import { Card } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom"
+
 import Category from "../../../models/Category";
 import MobXContext from "../../../stores/MobXContext";
 import './CategoriesPage.scss';
@@ -15,6 +16,7 @@ const CategoriesPage: React.FC<ICategoriesPageProps> = observer(function Categor
 
     const { categoryStore } = useContext(MobXContext);
     const [categories, setCategories] = useState<Category[]>(null);
+    const navigate = useNavigate();
 
     const category: Category = {
         id: 1,
@@ -24,7 +26,7 @@ const CategoriesPage: React.FC<ICategoriesPageProps> = observer(function Categor
         description: "test"
     };
 
-    function createCategory(category: Category) {
+    /* function createCategory(category: Category) {
         const createCategoryAsync = async () => {
             try {
                 await categoryStore.createCategory(category)
@@ -34,7 +36,7 @@ const CategoriesPage: React.FC<ICategoriesPageProps> = observer(function Categor
             }
         }
         createCategoryAsync();
-    }
+    }*/ 
 
     useEffect(() => {
         const getCategoryModel = async () => {
@@ -48,22 +50,26 @@ const CategoriesPage: React.FC<ICategoriesPageProps> = observer(function Categor
         getCategoryModel();
     }, [categoryStore])
 
+    function handleClick(category : Category) {
+        navigate('/subcategories/'+category.id);
+    }
+
     if (categories) {
         return (
-
             <div className="container">
-            {categories.map((index) => (
+            {categories.map((category) => (
               <div className="col-20">
-                <Card border="primary">
+                <Card border="primary"  onClick={() => handleClick(category)}>
                   <Card.Body>
-                    <img src={index.imageUrl} className='img-fluid shadow-4' alt='...'/>
+                    <img src={category.imageUrl} className='img-fluid shadow-4' alt='...'/>
                   </Card.Body>
-                  <Card.Footer> {index.name.toString()}</Card.Footer>
+                  <Card.Footer> {category.name.toString()}</Card.Footer>
                 </Card>
               </div>
             ))}
           </div>
-        ) }
+        ) 
+    }
 
    /* if (categories) {
         return (
