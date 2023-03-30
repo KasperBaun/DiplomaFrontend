30291b9ee352b278661bddb4a9011dce1fa54f94
Kasper Baun <kasperbaun@hotmail.com>
2023-03-30 17:47:20 +0200

    The quickest of fixes

--- src/pages/webshop/subcategory/SubcategoriesPage.tsx
+++ src/pages/webshop/subcategory/SubcategoriesPage.tsx
@@ -1,23 +1,24 @@
 import { observer } from "mobx-react-lite"
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { useContext } from "react";
 import MobXContext from "../../../stores/MobXContext";
 import { Card, Container } from "react-bootstrap";
 import SubCategory from "@models/SubCategory";
 
 const SubCategoriesPage: React.FC = observer(function SubCategoriesPage(this: any) {
-    const { subCategoryStore } = useContext(MobXContext);   
+    const { subCategoryStore } = useContext(MobXContext);
     const { languageStore } = useContext(MobXContext);
 
-    let {id} = useParams();
-    const location = useLocation(); 
-    const {name} = location.state; 
+    let { id } = useParams();
+    const location = useLocation();
+    const navigate = useNavigate();
+    const { name } = location.state;
     const subCategories = (subCategoryStore.subCategoriesByCategoryID(Number(id)))
 
-    function handleClick(subCategory: SubCategory, name : String) {
-       // navigate('/subcategories/' + subCategory.id , {state: { name }})
+    function handleClick(subCategory: SubCategory, name: String) {
+        navigate('/productList/' + subCategory.id, { state: { name } })
     }
-    if (subCategories.length > 0)
+    if (subCategories && subCategories.length > 0)
         return (
             <Container>
                 <h1>{name}</h1>
@@ -26,7 +27,7 @@ const SubCategoriesPage: React.FC = observer(function SubCategoriesPage(this: an
                         <div className="col-20-cat">
                             <Card className="category" border="primary" onClick={() => handleClick(subCategory, subCategory.name)}>
                                 <Card.Body className="category">
-                                    <img src={subCategory.imageUrl} className='img-fluid shadow-4' style={{height: '13.5rem', width: 'auto'}} alt='...' />
+                                    <img src={subCategory.imageUrl} className='img-fluid shadow-4' style={{ height: '13.5rem', width: 'auto' }} alt='...' />
                                 </Card.Body>
                                 <Card.Footer className="category"> {subCategory.name.toString()}</Card.Footer>
                             </Card>
@@ -36,7 +37,7 @@ const SubCategoriesPage: React.FC = observer(function SubCategoriesPage(this: an
             </Container>
         )
     else
-        return(
+        return (
             <Container>
                 <div>
                     <h1>{name}</h1>
