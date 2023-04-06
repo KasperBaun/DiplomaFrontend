99d04f1cfb3d4064cc73b92d4ff5f3ad6eb96340
Kasper Baun <kasperbaun@hotmail.com>
2023-04-06 14:53:48 +0200

    minor warning fixes

--- src/pages/backoffice/Dashboard/components/RecentSales.tsx
+++ src/pages/backoffice/Dashboard/components/RecentSales.tsx
@@ -1,59 +1,59 @@
 import { Container, Link, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import MobXContext from "@stores/MobXContext";
 import { observer } from "mobx-react-lite";
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { Spinner } from "react-bootstrap";
 
-class Payment{
-  id: number;
-  datePaid? : Date;
-  amount? : number;
-  approved? : number;
-  method? : string;
-}
+// class Payment{
+//   id: number;
+//   datePaid? : Date;
+//   amount? : number;
+//   approved? : number;
+//   method? : string;
+// }
 
 interface IProps {
-    title : string;
-    datePaid : string;
-    approved : string;
-    amount : string;
-    tableButton : string;
-    currencyId : string;
-    method : string;
+  title: string;
+  datePaid: string;
+  approved: string;
+  amount: string;
+  tableButton: string;
+  currencyId: string;
+  method: string;
 }
 
-const RecentSalesList = ( props : IProps ) => {
+const RecentSalesList = (props: IProps) => {
   const { paymentStore } = useContext(MobXContext);
 
   function preventDefault(event: React.MouseEvent) {
-      event.preventDefault();
+    event.preventDefault();
   }
 
-  if(paymentStore.Payments) {
+  if (paymentStore.Payments) {
     return (
       <React.Fragment>
         <h3>{props.title}</h3>
         <TableContainer sx={{ height: 225 }}>
-        <Table size="small">
-          <TableHead>
-            <TableRow>
-              <TableCell>{props.datePaid}</TableCell>
-              <TableCell>{props.approved}</TableCell>
-              <TableCell>{props.method}</TableCell>
-              <TableCell align="right">{props.amount}</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody style={{ overflowY: 'auto', maxHeight: '15rem' }}>
-            {paymentStore.Payments.map((payment) => (
-              <TableRow key={payment.id}>
-                <TableCell>{ payment.datePaid.toString() }</TableCell>
-                <TableCell align="center">{ payment.approved }</TableCell>
-                <TableCell align="center">{ payment.method }</TableCell>
-                <TableCell align="right">{`${payment.amount} DKK`}</TableCell>
+          <Table size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell>{props.datePaid}</TableCell>
+                <TableCell>{props.approved}</TableCell>
+                <TableCell>{props.method}</TableCell>
+                <TableCell align="right">{props.amount}</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHead>
+            <TableBody style={{ overflowY: 'auto', maxHeight: '15rem' }}>
+              {paymentStore.Payments.map((payment) => (
+                <TableRow key={payment.id}>
+                  <TableCell>{payment.datePaid.toString()}</TableCell>
+                  <TableCell align="center">{payment.approved}</TableCell>
+                  <TableCell align="center">{payment.method}</TableCell>
+                  <TableCell align="right">{`${payment.amount} DKK`}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </TableContainer>
         <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
           {props.tableButton}
@@ -63,11 +63,11 @@ const RecentSalesList = ( props : IProps ) => {
   } else {
     return (
       <Container style={{ textAlign: "center", padding: "15rem" }}>
-          <Spinner animation="grow" variant="secondary" /> Loading...
+        <Spinner animation="grow" variant="secondary" /> Loading...
       </Container>
     )
   }
-  
+
 }
 
 export default observer(RecentSalesList);
--- src/pages/backoffice/Dashboard/dashboard.tsx
+++ src/pages/backoffice/Dashboard/dashboard.tsx
@@ -1,49 +1,49 @@
 import { Container, Grid } from "@mui/material";
 import MobXContext from "@stores/MobXContext";
 import { useContext } from "react";
+import "./css/dashboard.scss";
 import EconomyWidget from "./components/EconomyWidget";
 import RecentSalesList from "./components/RecentSales";
-import "./css/dashboard.scss";
 
-const BackOfficeDashboard = () => {
+const Dashboard = () => {
     const { languageStore } = useContext(MobXContext);
 
     return (
-            <Grid container spacing={2}>
-                <Grid className="DashBoardGridContainer" item xs={12}>
-                    <EconomyWidget title={languageStore.currentLanguage.EconomyWidgetTitle} salgsformat={languageStore.currentLanguage.EconomyWidgetSalesFormat}/>
-                </Grid>
-                <Grid className="DashBoardGridContainer" item xs={12}>
-                    <Container  maxWidth="xl" className="DashBoardContainer">
-                        <RecentSalesList 
-                            title={languageStore.currentLanguage.RecentSalesWidgetTitle} 
-                            datePaid={languageStore.currentLanguage.RecentSalesDatePaid}
-                            approved={languageStore.currentLanguage.RecentSalesApproved}
-                            amount={languageStore.currentLanguage.RecentSalesAmount}
-                            tableButton={languageStore.currentLanguage.RecentSalesNavButton}
-                            currencyId={languageStore.currentLanguage.RecentSalesCurrencyId}
-                            method={languageStore.currentLanguage.RecentSalesMethod}
-                        />
-                    </Container>
-                </Grid>
-                
-                <Grid className="DashBoardGridContainer" item xs>
-                    <Container className="DashBoardContainer">
-                        <h3>Salg Total</h3>
-                    </Container>
-                </Grid>
-                <Grid className="DashBoardGridContainer" item xs>
-                    <Container className="DashBoardContainer">
-                        <h3>Køb Total</h3>
-                    </Container>
-                </Grid>
-                <Grid className="DashBoardGridContainer" item xs>
-                    <Container className="DashBoardContainer">
-                        <h3>Inventar</h3>
-                    </Container>
-                </Grid>
+        <Grid container spacing={2}>
+            <Grid className="DashBoardGridContainer" item xs={12}>
+                <EconomyWidget title={languageStore.currentLanguage.EconomyWidgetTitle} salgsformat={languageStore.currentLanguage.EconomyWidgetSalesFormat} />
             </Grid>
+            <Grid className="DashBoardGridContainer" item xs={12}>
+                <Container maxWidth="xl" className="DashBoardContainer">
+                    <RecentSalesList
+                        title={languageStore.currentLanguage.RecentSalesWidgetTitle}
+                        datePaid={languageStore.currentLanguage.RecentSalesDatePaid}
+                        approved={languageStore.currentLanguage.RecentSalesApproved}
+                        amount={languageStore.currentLanguage.RecentSalesAmount}
+                        tableButton={languageStore.currentLanguage.RecentSalesNavButton}
+                        currencyId={languageStore.currentLanguage.RecentSalesCurrencyId}
+                        method={languageStore.currentLanguage.RecentSalesMethod}
+                    />
+                </Container>
+            </Grid>
+
+            <Grid className="DashBoardGridContainer" item xs>
+                <Container className="DashBoardContainer">
+                    <h3>Salg Total</h3>
+                </Container>
+            </Grid>
+            <Grid className="DashBoardGridContainer" item xs>
+                <Container className="DashBoardContainer">
+                    <h3>Køb Total</h3>
+                </Container>
+            </Grid>
+            <Grid className="DashBoardGridContainer" item xs>
+                <Container className="DashBoardContainer">
+                    <h3>Inventar</h3>
+                </Container>
+            </Grid>
+        </Grid>
     )
 }
 
-export default BackOfficeDashboard;
+export default Dashboard;
