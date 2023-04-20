841f1f6436277e9b6dbd4459cc4ac1a4ecbc2ea5
Simon Fridolf <fridolf.lindhard@gmail.com>
2023-04-20 16:21:11 +0200

    sext basket additions - almost done

--- src/pages/webshop/basket/BasketPage.tsx
+++ src/pages/webshop/basket/BasketPage.tsx
@@ -3,6 +3,10 @@ import { observer } from "mobx-react-lite"
 import { useContext} from "react";
 import MobXContext from "@stores/MobXContext";
 import BasketPageItem from "./BasketPageItem";
+import Card from '@mui/material/Card';
+import { Margin } from "@mui/icons-material";
+import { Button, Divider } from "@mui/material";
+
 
 interface IBasketPageProps {
 
@@ -16,13 +20,31 @@ const BasketPage: React.FC<IBasketPageProps> = observer(function BasketPage(prop
     return (
         <Grid container spacing={2}>
             <Grid item xs={12} sm={12} md={8} lg={8} xl={8}>
-                <h3 style={{paddingTop:"1rem", paddingLeft:"1rem"}}>{languageStore.currentLanguage.yourBasket}</h3>
-                {basketStore.Basket.map((item, index) => (
-                <BasketPageItem key={item.id + index} item={item} />
-              ))}
+                    <Card style={{marginTop:"2rem"}}>    
+                        <h3 style={{paddingTop:"1rem", paddingLeft:"1rem"}}>{languageStore.currentLanguage.yourBasket}</h3>
+                        {basketStore.Basket.map((item, index) => (
+                        <BasketPageItem key={item.id + index} item={item} />
+                    ))}
+                    </Card>
             </Grid>
             <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
-                <h3 style={{paddingTop:"1rem"}}>{languageStore.currentLanguage.yourTotal}</h3>
+                <Card style={{marginTop:"2rem"}}>
+                    <h3 style={{paddingTop:"1rem"}}>{languageStore.currentLanguage.yourTotal}</h3>
+                    <div className="b_PriceText">Subtoal: </div>
+                    <div className="b_PriceText">Levering:  </div>
+                    <Divider/>
+                    <div className="b_PriceText" style={{fontWeight:"700"}}>Pris i alt (inklusiv moms): </div>
+                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '1rem'}}>
+                        <Button className="checkoutButton" variant="contained" style={{width: '50%', minHeight: '2rem', margin:'1.5rem',}}>Check out</Button>
+                    </div>
+                </Card>
+
+                <Card style={{marginTop:"2rem"}}>
+                <h5 style={{paddingTop:"1rem"}}>Tilføj rabatkode:</h5>
+
+                </Card>
+
+
             </Grid>  
         </Grid>
     )
