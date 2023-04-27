71b43278f68b3b53fac4df2f8583a1d92c236a39
Simon Fridolf <fridolf.lindhard@gmail.com>
2023-04-27 11:12:42 +0200

    New order models + ordersStore start

--- src/models/Orders.ts
+++ src/models/Orders.ts
@@ -1,11 +1,13 @@
+import OrderElements from "./OrderElements";
+
 export default class Order {
     id: number;
     customerId: number;
     paymentId: number;
-    productItemId: number;
     paymentStatus: string;
     deliveryStatus: string;
     discountCode: string | null;
     active: Boolean;
+    orderElements: OrderElements[];
   }
   
