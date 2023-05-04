1fef8b829f7fa661bec123d4a2a9d8ecd9250343
Kasper Baun <kasperbaun@hotmail.com>
2023-05-04 15:42:01 +0200

    footer fixed

--- src/pages/webshop/Webshop.tsx
+++ src/pages/webshop/Webshop.tsx
@@ -1,33 +1,30 @@
-import Footer from "@components/footer/Footer";
+import { Footer } from "@components/footer/Footer";
 import Header from "@components/header/Header";
 import Loading from "@components/loading/Loading";
 import MobXContext, { IMobXContext } from "@stores/MobXContext";
 import { Constants } from "@utils/Constants";
 import { observer } from "mobx-react-lite";
-import { FunctionComponent, useContext } from "react";
+import { useContext } from "react";
 import { Container } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 
-interface WebshopProps {
-
-}
-
-const Webshop: FunctionComponent<WebshopProps> = observer(function Webshop() {
+const Webshop: React.FC = observer(function Webshop() {
 
     const { rootStore } = useContext<IMobXContext>(MobXContext);
 
     if (!rootStore.isLoaded) {
         return (
             <Loading
-                size={100}
+                size={50}
                 color={Constants.primaryColor}
+                loadingText="Loading..."
             />
         )
     } else {
         return (
             <div>
                 <Header />
-                <Container style={{ minHeight:"70vh", display: 'flex', justifyContent: 'center', height: '100%', width: '100%', marginTop: 'auto' }}>
+                <Container style={{ minHeight: "80vh", display: 'flex', justifyContent: 'center', height: '100%', width: '100%', marginTop: 'auto' }}>
                     <Outlet />
                 </Container>
                 <Footer />
