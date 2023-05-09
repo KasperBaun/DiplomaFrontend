037df03539de3f5c860fe4e6375adee9cce39be4
Kasper Baun <kasperbaun@hotmail.com>
2023-05-09 21:52:17 +0200

    removed console.log leftover

--- src/pages/webshop/categories/CategoriesPage.tsx
+++ src/pages/webshop/categories/CategoriesPage.tsx
@@ -1,9 +1,8 @@
 import { observer } from "mobx-react-lite"
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom"
-import { Container, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 import MobXContext from "../../../stores/MobXContext";
-import './CategoriesPage.scss';
 import CategoryCardWeb from "./CategoryCardWeb";
 import Loading from "@components/loading/Loading";
 
@@ -19,7 +18,6 @@ const CategoriesPage: React.FC = observer(function Categories() {
     if (webshopStore.Categories) {
         return (
             <Grid container display={'flex'} flexWrap={'wrap'} alignItems={'flex-start'}>
-
                 {webshopStore.Categories.map((category, index) => (
                     <Grid item xs={12} sm={6} md={4} lg={3} xl={3} padding={1} display='flex' key={"BackofficeCategoryCardItem" + index}>
                         <CategoryCardWeb category={category} onCardClicked={handleClick} />
